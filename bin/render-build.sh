c2fd07a940650bc2dde99418614fe0d550458e37
prathameshchavan27 <prathameshlchavan@gmail.com>
2025-08-19 19:54:15 +0530

    edit render-build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
+# Install Ruby gems from Gemfile.lock
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
 
-bin/rails db:migrate
+# Precompile assets
+bundle exec rails assets:precompile
+
+# Run database migrations
+bundle exec rails db:migrate
